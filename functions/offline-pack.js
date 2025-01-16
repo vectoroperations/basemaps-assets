7d7553f67e4a5d44a28e52eb506bd34e4017b944
michaelmedellin <13091665+michaelmedellin@users.noreply.github.com>
2025-01-16 10:31:36 -0500

    Delete functions/offline-pack.js

--- functions/offline-pack.js
+++
@@ -1,8 +0,0 @@
-export async function onRequestGet(ctx) {
-  const path = new URL(ctx.request.url).pathname.replace("/offline-pack/", "");
-  const file = await ctx.env.BUCKET.get(path);
-  if (!file) return new Response(null, { status: 404 });
-  return new Response(file.body, {
-    headers: { "Content-Type": file.httpMetadata.contentType },
-  });
-}
